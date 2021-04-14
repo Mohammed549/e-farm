cddd68ba317cc565f1e15eb17826cf04b101c4ed
Mohammed549 <33037257+Mohammed549@users.noreply.github.com>
2021-04-14 20:08:32 +0300

    Update products.py

--- backend/base/products.py
+++ backend/base/products.py
@@ -11,4 +11,10 @@ products = [
         "category": "fruit",
         "description": "tomatos gonna potate",
     },
+    {
+        "_id": "3",
+        "name": "nuts",
+        "category": "fruit",
+        "description": "fresh nuts",
+    },
 ]
