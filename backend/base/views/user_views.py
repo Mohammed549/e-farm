1af09b442d539fe84bff4c73411d9efd80bc07af
hakxran <hdyillik@gmail.com>
2021-04-24 18:38:21 +0300

    Some_Change

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from django.contrib.auth import get_user_model
-from .models import Product
-from .products import products
-from .serializer import ProductSerializer, UserSerializer, UserSerializerWithToken
+
+from base.serializer import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from django.contrib.auth.hashers import make_password
+from rest_framework import status 
 
-#sdfdfgfghgdhfjfghjfhjkfhdfgdfgdafgsfghsfghfg
-#dfgfdsghdgfhjdghjfghjfhgjkfhjkjfhjkfhjkfjhk
+
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
@@ -35,15 +34,18 @@ class MyTokenObtainPairView(TokenObtainPairView):
 @api_view(["POST"])
 def registerUser(request):
     data = request.data
-
-    user = get_user_model().objects.create(
-        first_name = data['name'],
-        username = data['email'],
-        email = data['email'],
-        password = make_password(data['password'])
-    )
-    serializer = UserSerializerWithToken(user,many=False)
-    return Response(serializer.data)
+    try:
+        user = get_user_model().objects.create(
+            first_name = data['name'],
+            username = data['email'],
+            email = data['email'],
+            password = make_password(data['password'])
+        )
+        serializer = UserSerializerWithToken(user,many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail':'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
 
@@ -60,19 +62,4 @@ def getUserProfile(request):
 def getUsers(request):
     users = get_user_model().objects.all()
     serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
-
-
-@api_view(["GET"])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
-
-
-@api_view(["GET"])
-def getProduct(request, pk):
-    product = Product.objects.get(_id=pk)
-    serializer = ProductSerializer(product, many=False)
-
     return Response(serializer.data)
